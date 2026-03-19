8a8a144fe691159b0a6bda7c7bf412fd2a8e809c
D0do777 <justeforbusiness1@gmail.com>
2026-03-19 19:11:54 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function gameLoop()
+{
+update();
+draw();
+
+requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
